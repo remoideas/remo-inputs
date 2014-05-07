7c7fe91617b5eb99e527433563ef2ab1fcacd51f
Antonio <antremo@gmail.com>
2014-05-07 10:12:43 -0500

    1.5

--- remo-inputs.php
+++ remo-inputs.php
@@ -5,7 +5,7 @@ Description: Inputs para crear fomularios personalizados en Sitios.
 Plugin URI: http://www.remoideas.com
 Author: Antonio Reyes
 Author URI: http://www.remoideas.com
-Version: 1.2
+Version: 1.5
 License: Reservados
 Text Domain: RemoIdeas.com
 Domain Path: Domain Path
@@ -26,6 +26,7 @@ if (is_admin()) { // note the use of is_admin() to double check that this is hap
         'sslverify' => true, // wether WP should check the validity of the SSL cert when getting an update, see https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/2 and https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/4 for details
         'requires' => '3.0', // which version of WordPress does your plugin require?
         'tested' => '3.3', // which version of WordPress is your plugin tested up to?
+        'readme' => 'README.md', // which file to use as the readme for the version number
         'access_token' => '', // Access private repositories by authorizing under Appearance > Github Updates when this example plugin is installed
     );
     new WP_GitHub_Updater($config);
