444c915f79aeacd691a995089aca025504a3be37
Antonio <antremo@gmail.com>
2014-05-07 14:06:48 -0500

    Regreso al update.php

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ REMO INPUTS
 ==============
 
 This Wordpress Plugin work like a cms to create and save fields on custom tables inside db wp. 
+
+~Current Version:1.0.0~
--- remo-inputs.php
+++ remo-inputs.php
@@ -5,13 +5,33 @@ Description: Inputs para crear fomularios personalizados en Sitios.
 Plugin URI: http://www.remoideas.com
 Author: Antonio Reyes
 Author URI: http://www.remoideas.com
-Version: 1.0.2
+Version: 1.0.0
 License: Reservados
 Text Domain: RemoIdeas.com
-Domain Path: remo-ideas
+Domain Path: Domain Path
 GitHub Plugin URI: https://github.com/remoideas/remo-inputs
 GitHub Branch:     master
 */
+
+include_once('updater.php');
+
+
+if (is_admin()) { // note the use of is_admin() to double check that this is happening in the admin
+    $config = array(
+        'slug' => plugin_basename(__FILE__), // this is the slug of your plugin
+        'proper_folder_name' => 'remo-inputs', // this is the name of the folder your plugin lives in
+        'api_url' => 'https://api.github.com/repos/remoideas/remo-inputs', // the github API url of your github repo
+        'raw_url' => 'https://raw.github.com/remoideas/remo-inputs/master', // the github raw url of your github repo
+        'github_url' => 'https://github.com/remoideas/remo-inputs', // the github url of your github repo
+        'zip_url' => 'https://github.com/remoideas/remo-inputs/zipball/master', // the zip url of the github repo
+        'sslverify' => true, // wether WP should check the validity of the SSL cert when getting an update, see https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/2 and https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/4 for details
+        'requires' => '3.0', // which version of WordPress does your plugin require?
+        'tested' => '3.3', // which version of WordPress is your plugin tested up to?
+        'readme' => 'README.md', // which file to use as the readme for the version number
+        'access_token' => '', // Access private repositories by authorizing under Appearance > Github Updates when this example plugin is installed
+    );
+    new WP_GitHub_Updater($config);
+}
 
 
 //Defino la ruta del plugin
