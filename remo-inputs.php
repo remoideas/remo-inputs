fc3ce059cc81e2c072a60bb93233a1011040104e
Antonio <antremo@gmail.com>
2014-05-07 10:00:43 -0500

    Update update.php

--- remo-inputs.php
+++ remo-inputs.php
@@ -26,7 +26,6 @@ if (is_admin()) { // note the use of is_admin() to double check that this is hap
         'sslverify' => true, // wether WP should check the validity of the SSL cert when getting an update, see https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/2 and https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/4 for details
         'requires' => '3.0', // which version of WordPress does your plugin require?
         'tested' => '3.3', // which version of WordPress is your plugin tested up to?
-        'readme' => 'README.md', // which file to use as the readme for the version number
         'access_token' => '', // Access private repositories by authorizing under Appearance > Github Updates when this example plugin is installed
     );
     new WP_GitHub_Updater($config);
